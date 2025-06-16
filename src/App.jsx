0e27eca4fcd48334d40dca752e3a3ff1bbf99f16
Carlos <carloscr95r@gmail.com>
2025-06-16 11:01:41 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,41 +14,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';
 
 function App() {
-
-// Configuración y definición de las rutas
-
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<ItemListContainer title="Listado de productos" />} />
-//         <Route path="/explorar" element={<div>Página Explorar</div>} />
-//         <Route path="/hombre" element={<div>Página Hombre</div>} />
-//         <Route path="/mujer" element={<div>Página Mujer</div>} />
-//         <Route path="/nino" element={<div>Página Niño</div>} />
-//         <Route path="/disciplinas" element={<div>Página Disciplinas</div>} />
-//         <Route path="/marcas" element={<div>Página Marcas</div>} />
-//         <Route path="/comunidad" element={<div>Página Comunidad</div>} />
-//         <Route path="/nosotros" element={<div>Página Nosotros</div>} />
-//         <Route path="/contacto" element={<div>Página Contacto</div>} />
-//         <Route path="/cart" element={<div>Carrito de compras</div>} />
-//         <Route path="*" element={<ErrorPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-return (
+  return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={ <ItemListContainer titulo='Todos los productos'/>}/>
-      <Route path='/tienda' element={ <ItemListContainer titulo='Todos los productos'/>}/>
-      <Route path='/category/:categoryId' element={ <ItemListContainer titulo='Category'/>}/>
-      <Route path='/gender/:gender' element={ <ItemListContainer titulo='Gender'/>}/>
-      <Route path='/product/:id' element={<ItemDetailContainer/>}/>
-      <Route path='*' element={<ErrorPage/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<ItemListContainer titulo='Todos los productos' />} />
+        <Route path='/tienda' element={<ItemListContainer titulo='Todos los productos' />} />
+        <Route path='/category/:categoryId' element={<ItemListContainer titulo='Productos por categoría' />} />
+        <Route path='/gender/:gender' element={<ItemListContainer titulo='Productos por género' />} />
+        <Route path='/product/:id' element={<ItemDetailContainer />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
