ede8aaa7f640e844f23f5b1236df96f9d8fc1565
Carlos <carloscr95r@gmail.com>
2025-06-18 16:00:06 -0300

    cart updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import CartItemContainer from "../components/Cart/CartItemContainer";
+import CartSummary from "../components/Cart/CartSummary";
 // TODO: Armar sección de carrito completa
 
 const Cart = () => {
   return (
-    <div className="container mt-5">
-      <h1 className="my-4 text-center">Tu carrito de compras</h1>
-      <CartItemContainer />
-    </div>
+    <>
+      <div className="mt-5">&nbsp;</div>
+      <div className="container">
+        <h1 className="my-4 text-center">Tu carrito de compras</h1>
+        <div className="row g-4">
+          <CartItemContainer />
+          <CartSummary />
+        </div>
+      </div>
+    </>
   );
 };
 
