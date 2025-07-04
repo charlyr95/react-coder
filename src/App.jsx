f77a6a0e20cdcd1b213f8fa6d0cdf818f9c0704b
Carlos <carloscr95r@gmail.com>
2025-07-04 20:07:04 -0300

    limpieza de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { ToastContainer } from 'react-toastify'
 // css
 import 'react-toastify/dist/ReactToastify.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/App.css'
 
 // components
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-// import './css/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
