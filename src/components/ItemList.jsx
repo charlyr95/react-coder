08ec89e8e899ca93722ab38b6f485ef102c2bbb0
Carlos <carloscr95r@gmail.com>
2025-07-09 23:58:49 -0300

    padding and margin fix

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,7 @@ import Item from './Item'
 
 const ItemList = ({data}) => {
   return (
-    <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4 mb-5">
+    <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4 m-0">
         {data.map((prod)=> <Item key={prod.id} prod={prod}/>)}
     </div>
   )
